aab2b3fc38d324a21e46cc6c6b9020bae0169063
Ashley Tucker <ashleyst35@aol.com>
2022-08-22 13:41:18 -0400

    car ex 1

---
+++ Car/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using ClassesExercise1; 
+
+
+Car myCar = new Car();
+myCar.Make = "Mercedes";
+myCar.Model = "G550";
+myCar.Year = 2022;
+
+Console.WriteLine(myCar.Make);   
+Console.WriteLine(myCar.Model);
+Console.WriteLine(myCar.Year);
+
+
+
+
+
+
+
+
+
+
+
+
